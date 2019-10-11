4f79781221044a3981d7672c6f050acd1bccd72f
EliTu <eliad91@gmail.com>
2019-10-11 20:06:59 +0300

    Testing the Layout component

--- src/components/containers/Layout/Layout.test.tsx
+++ src/components/containers/Layout/Layout.test.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Layout from './Layout';
+import Backdrop from '../../stateless/UI/Backdrop/Backdrop';
 
-describe('Layout component', () => {});
+describe('Layout component', () => {
+	let component;
+
+	beforeEach(() => {
+		component = shallow(<Layout />);
+	});
+
+	it('should render without errors', () => {
+		expect(component).toMatchSnapshot();
+		expect(component.length).toBe(1);
+		expect(component.length).not.toBe(2);
+		expect(component).toHaveClassName('LayoutStyles');
+	});
+
+	it('should render the Backdrop component without errors', () => {
+		expect(component).toContainReact(<Backdrop isDisplayed />);
+	});
+
+	it('should render a main tag that contains the children prop', () => {
+		const main = component.find('main');
+
+		expect(main.length).toBe(1);
+		expect(main.children()).toBeTruthy();
+		expect(main.props('children')).toBeTruthy();
+	});
+});
--- src/components/containers/Layout/Layout.tsx
+++ src/components/containers/Layout/Layout.tsx
@@ -6,6 +6,7 @@ interface IProps {}
 
 const Layout: React.FC<IProps> = ({ children }) => {
 	const { LayoutStyles } = styles;
+
 	return (
 		<div className={LayoutStyles}>
 			<Backdrop isDisplayed />
