e9a35945ee62c1c501ccb206883a70e69b1ce6f0
EliTu <eliad91@gmail.com>
2019-10-11 10:40:32 +0300

    Adding a Navbar component

---
+++ src/components/stateless/UI/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+	const { Navbar } = styles;
+
+	return <div className={Navbar}>
+        
+    </div>;
+};
+
+export default Navbar;
