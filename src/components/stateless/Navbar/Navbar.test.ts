820394382f4769ca900e08ad9a65e998f36fc8b1
EliTu <eliad91@gmail.com>
2019-10-11 11:00:20 +0300

    Configuring Jest and seting up for testing

---
+++ src/components/stateless/Navbar/Navbar.test.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Navbar from './Navbar';
+
+describe('Navbar component', () => {
+	let component;
+	beforeEach(() => (component = shallow(Navbar)));
+
+	it('should render without errors', () => {});
+});
