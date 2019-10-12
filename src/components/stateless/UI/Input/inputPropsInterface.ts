84c15f98988842ff4e72ba3e8880a83fb56d9fc4
EliTu <eliad91@gmail.com>
2019-10-12 14:08:10 +0300

    Finished testing the Input component. Added a test-utility function

---
+++ src/components/stateless/UI/Input/inputPropsInterface.ts
@@ -0,0 +1,23 @@
+// The Input component prop type TS interface
+export interface IInputProps {
+	elementType: string;
+	elementConfig: {
+		type: string;
+		placeholder: string;
+		label: string;
+		options?: [{ value: string; displayValue: string; disabled: boolean }];
+		min?: number;
+		max?: number;
+	};
+	value: string | number;
+	validation?: {
+		required: boolean;
+		valid: boolean;
+		hasUserInput: boolean;
+		errorMessage: string;
+	};
+	isFocused: boolean;
+	data: string;
+	handleChange: (event: object) => void;
+	handleEnterPress: (event: object) => void;
+}
---
+++ src/utilities/test-utilities/findByTestAttr.ts
@@ -0,0 +1,7 @@
+import { ShallowWrapper } from 'enzyme';
+
+// Used to find a specific DOM element that is specified with the 'data-test' attribute to better traverse nodes while testing:
+export const findByTestAttr = (component: ShallowWrapper, attr: string) => {
+	const wrapper = component.find(`[data-test='${attr}']`);
+	return wrapper;
+};
