c708be3230f0179ef9a4ad44625ed61fee7d2ca1
EliTu <eliad91@gmail.com>
2019-10-13 15:59:21 +0300

    Adding mock info for the SelectedWeather component

--- src/components/containers/WeatherDisplayContainer/SelectedWeather/SelectedWeather.tsx
+++ src/components/containers/WeatherDisplayContainer/SelectedWeather/SelectedWeather.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import styles from './SelectedWeather.module.css';
 
 const SelectedWeather: React.FC = () => {
-	const { SelectedWeatherStyles } = styles;
-	return <div className={SelectedWeatherStyles}></div>;
+	const { SelectedWeatherStyles, testDiv } = styles;
+	return (
+		<div className={SelectedWeatherStyles}>
+			<div className={testDiv}>Img</div>
+			<p>Current weather forcast</p>
+		</div>
+	);
 };
 
 export default SelectedWeather;
