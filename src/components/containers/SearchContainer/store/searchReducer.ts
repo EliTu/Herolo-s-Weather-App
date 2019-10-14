efe8c680ff50254364dffd9e1bd210942320be15
EliTu <eliad91@gmail.com>
2019-10-14 19:07:48 +0300

    Working on the searchReducer

--- src/components/containers/SearchContainer/store/searchReducer.ts
+++ src/components/containers/SearchContainer/store/searchReducer.ts
@@ -3,17 +3,22 @@ import {
 	SEARCH_REQUEST_INIT,
 	SEARCH_REQUEST_SUCCESS,
 	SEARCH_REQUEST_FAIL,
+	SearchResultActionTypes,
 } from './types';
 
 const INITIAL_STATE: SearchResult = {
 	isLoading: false,
+	error: '',
 	key: '',
 	type: '',
 	localizedName: '',
 };
 
-const searchReducer = (state = INITIAL_STATE, type): SearchResult => {
-	switch (type.name) {
+const searchReducer = (
+	state = INITIAL_STATE,
+	action: SearchResultActionTypes
+): SearchResult => {
+	switch (action.type) {
 		case SEARCH_REQUEST_INIT:
 			return { ...state, ...payload };
 		case SEARCH_REQUEST_SUCCESS:
