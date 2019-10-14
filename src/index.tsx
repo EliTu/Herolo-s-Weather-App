2730ad0e5179119747aa41db5bb84139399b9829
EliTu <eliad91@gmail.com>
2019-10-14 16:34:06 +0300

    Working on the Redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore, compose, Reducer } from 'redux';
+import { Provider } from 'react-redux';
+import appReducer from './store/appReducer';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Enabling the Redux Dev tools for development mode:
+const composeEnhancers =
+	(window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const store: (reducer: Reducer, composeEnhance: any) => void = createStore(
+// 	appReducer,
+// 	composeEnhancers
+// );
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/appReducer.ts
@@ -0,0 +1,5 @@
+const INITIAL_STATE = {};
+
+const appReducer = (state = INITIAL_STATE, action) => {};
+
+export default appReducer;
