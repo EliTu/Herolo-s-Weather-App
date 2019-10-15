5c559d5db2bc46ed372c86538e6358dcdddb43dc
EliTu <eliad91@gmail.com>
2019-10-15 12:22:51 +0300

    Updating the SearchContainer tests

--- src/components/containers/SearchContainer/SearchContainer.test.tsx
+++ src/components/containers/SearchContainer/SearchContainer.test.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
 import { SearchContainer } from './SearchContainer';
+import Input from '../../stateless/UI/Input/Input';
+import Icon from '../../stateless/UI/Icon/Icon';
 
 describe('SearchContainer Component', () => {
 	let component: ShallowWrapper;
-	beforeEach(() => (component = shallow(<SearchContainer />)));
+	beforeEach(
+		() => (component = shallow(<SearchContainer httpRequest={() => {}} />))
+	);
 
 	it('should render without errors', () => {
 		expect(component).toMatchSnapshot();
@@ -13,7 +17,13 @@ describe('SearchContainer Component', () => {
 		expect(component).toHaveClassName('SearchContainerStyles');
 	});
 
-	it('should render an Input component without errors', () => {
-		expect(component.find('Input').length).toBe(1);
+	it('should render a div wrapper that contains an Input and an Icon components', () => {
+		expect(component.children().find('div').length).toBe(1);
+
+		expect(component.children().find(Input)).toExist();
+		expect(component.children().find(Input).length).toBe(1);
+
+		expect(component.children().find(Icon)).toExist();
+		expect(component.children().find(Icon).length).toBe(1);
 	});
 });
