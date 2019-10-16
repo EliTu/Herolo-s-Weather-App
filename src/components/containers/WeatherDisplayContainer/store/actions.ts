3c048d1a2a186fc7b4a5bbc0030f90699021b1da
EliTu <eliad91@gmail.com>
2019-10-17 02:03:41 +0300

    Set fiveDaysForecast action creators

--- src/components/containers/WeatherDisplayContainer/store/actions.ts
+++ src/components/containers/WeatherDisplayContainer/store/actions.ts
@@ -3,10 +3,15 @@ import {
 	CURRENT_WEATHER_SUCCESS,
 	CURRENT_WEATHER_FAIL,
 	CurrentWeatherActionTypes,
+	FIVE_DAYS_FORECAST_INIT,
+	FIVE_DAYS_FORECAST_SUCCESS,
+	FIVE_DAYS_FORECAST_FAIL,
+	FiveDaysForecastActionTypes,
 } from './types';
 import { Action, ActionCreator } from 'redux';
 import setAsyncGetRequest from '../../../../utilities/urls/urls';
 
+// CurrentWeather actions:
 export const currentWeatherInitAction: ActionCreator<
 	Action
 > = (): CurrentWeatherActionTypes => {
@@ -47,6 +52,55 @@ export const fireCurrentWeatherHttpRequest = (key: string) => {
 				dispatch(currentWeatherSuccessAction(weatherResult));
 			} catch (error) {
 				dispatch(currentWeatherFailAction(error));
+				console.log(error.message);
+			}
+	};
+};
+
+// FiveDaysForecast actions:
+export const fiveDaysForecastInitAction: ActionCreator<
+	Action
+> = (): FiveDaysForecastActionTypes => {
+	return {
+		type: FIVE_DAYS_FORECAST_INIT,
+	};
+};
+
+export const fiveDaysForecastSuccessAction: ActionCreator<Action> = (
+	results: {}[]
+): FiveDaysForecastActionTypes => {
+	return {
+		type: FIVE_DAYS_FORECAST_SUCCESS,
+		fiveDaysForecastResults: results,
+	};
+};
+
+export const fiveDaysForecastFailAction: ActionCreator<Action> = (
+	error: string
+): FiveDaysForecastActionTypes => {
+	return {
+		type: FIVE_DAYS_FORECAST_FAIL,
+		error: error,
+	};
+};
+
+// Thunk async action creator:
+export const fireFiveDaysForecastHttpRequest = (key: string) => {
+	return async (dispatch: any) => {
+		dispatch(fiveDaysForecastInitAction());
+		if (key)
+			try {
+				const result = await setAsyncGetRequest(
+					key,
+					'fiveDaysForecast'
+				);
+				const weatherResult;
+
+				console.log(weatherResult);
+
+				dispatch(fiveDaysForecastSuccessAction(weatherResult));
+			} catch (error) {
+				dispatch(fiveDaysForecastFailAction(error));
 				console.log(error.message);
 			}
 	};
