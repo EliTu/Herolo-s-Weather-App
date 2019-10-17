da89d153b057d277ab3e511abe75bd7632c97732
EliTu <eliad91@gmail.com>
2019-10-17 20:43:17 +0300

    Working on the ForecastCard component

--- src/components/containers/WeatherDisplayContainer/FiveDaysForecast/ForecastCard/ForecastCard.tsx
+++ src/components/containers/WeatherDisplayContainer/FiveDaysForecast/ForecastCard/ForecastCard.tsx
@@ -4,8 +4,8 @@ import styles from './Card.module.css';
 interface IProps {
 	mainHeading: string;
 	date: string;
-	maxTempData: {};
-	minTempData: {};
+	maxTempData: { Value: Number; Unit: string };
+	minTempData: { Value: Number; Unit: string };
 	description: string;
 	link: string;
 }
@@ -20,11 +20,20 @@ const ForecastCard: React.FC<IProps> = ({
 }) => {
 	const { CardStyles } = styles;
 
+	const { Value: maxVal, Unit: maxUnit } = maxTempData;
+	const maxTemp = `${maxVal} ${maxUnit}`;
+
+	const { Value: minVal, Unit: minUnit } = minTempData;
+	const minTemp = `${minVal} ${minUnit}`;
+
 	return (
 		<div className={CardStyles}>
-			<h3>{mainHeading}</h3>
-			<h4>{secondaryHeading}</h4>
-			<p>{info}</p>
+			<h3>
+				<a href={link}>{mainHeading}</a>
+			</h3>
+			<h4>{date}</h4>
+			<h4>{`${maxTemp} - ${minTemp}`}</h4>
+			<p>{description}</p>
 		</div>
 	);
 };
