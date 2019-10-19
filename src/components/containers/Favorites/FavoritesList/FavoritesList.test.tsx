99e23180b8497b1471d5699671d40a91e05e8421
EliTu <eliad91@gmail.com>
2019-10-20 02:00:49 +0300

    Updating tests

--- src/components/containers/Favorites/FavoritesList/FavoritesList.test.tsx
+++ src/components/containers/Favorites/FavoritesList/FavoritesList.test.tsx
@@ -5,9 +5,74 @@ import initialShallowRender from '../../../../utilities/test-utilities/initialSh
 
 describe('FavoritesList componenet', () => {
 	let component: ShallowWrapper;
-	beforeEach(() => (component = shallow(<FavoritesList />)));
+	const currentWeatherCallback = jest.fn();
+	const fiveDaysForecastCallback = jest.fn();
+	beforeEach(
+		() =>
+			(component = shallow(
+				<FavoritesList
+					favorites={[
+						{
+							key: '123',
+							cityName: 'munich',
+							countryName: 'germany',
+						},
+					]}
+					favoritesWeatherData={[
+						{
+							key: '123',
+							cityName: 'munich',
+							countryName: 'germany',
+							WeatherText: 'warm',
+							Temperature: { Metric: { Value: 23, Unit: 'c' } },
+						},
+						{
+							key: '456',
+							cityName: 'germering',
+							countryName: 'germany',
+							WeatherText: 'warm',
+							Temperature: { Metric: { Value: 24, Unit: 'c' } },
+						},
+					]}
+					history={{}}
+					getFavoritesWeatherData={() => {}}
+					getClickedItemWeatherCurrentData={currentWeatherCallback}
+					getClickedItemFiveDaysForecast={fiveDaysForecastCallback}
+				/>
+			))
+	);
 
 	it('should render without errors', () => {
 		initialShallowRender(component, 'FavoritesListStyles');
 	});
+
+	it('should render a div with the relevant favorite weather data', () => {
+		expect(component.children().find('div')).toExist();
+		expect(component.children().find('div').length).toBe(2);
+	});
+
+	it('should contain Card components inside the wrapping div', () => {
+		const card = component
+			.children()
+			.find('div')
+			.children();
+
+		expect(card.length).toBe(2);
+		expect(card.at(0)).toContain('Card');
+		expect(card.at(1)).toContain('Card');
+	});
+
+	it('should call both callback functions upon a click on one of the wrapping divs', () => {
+		component
+			.children()
+			.find('div')
+			.at(0)
+			.simulate('click');
+	});
+
+	expect(currentWeatherCallback).toHaveBeenCalled();
+	expect(currentWeatherCallback).toHaveBeenCalledTimes(1);
+
+	expect(fiveDaysForecastCallback).toHaveBeenCalled();
+	expect(fiveDaysForecastCallback).toHaveBeenCalledTimes(1);
 });
--- src/components/containers/Favorites/FavoritesList/FavoritesList.tsx
+++ src/components/containers/Favorites/FavoritesList/FavoritesList.tsx
@@ -5,31 +5,7 @@ import Card from '../../../display/UI/Card/Card';
 import { fireCurrentWeatherHttpRequest } from '../../WeatherDisplayContainer/store/actions';
 import { fireFiveDaysForecastHttpRequest } from '../../WeatherDisplayContainer/store/actions';
 import styles from './FavoritesList.module.css';
-
-interface IProps {
-	favorites: { key: string; cityName: string; countryName: string }[];
-	favoritesWeatherData: {
-		key: string;
-		cityName: string;
-		countryName: string;
-		WeatherText: string;
-		Temperature: { Metric: { Value: number; Unit: string } };
-	}[];
-	getFavoritesWeatherData: (
-		key: string,
-		countryName: string,
-		cityName: string,
-		dispatchIdentifier: string
-	) => void;
-	history: any;
-	getClickedItemWeatherCurrentData: (
-		key: string,
-		countryName: string,
-		cityName: string,
-		dispatchIdentifier: string
-	) => void;
-	getClickedItemFiveDaysForecast: (key: string) => void;
-}
+import { IProps } from './propsInterface';
 
 export const FavoritesList: React.FC<IProps> = ({
 	favorites,
